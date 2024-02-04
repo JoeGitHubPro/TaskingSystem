1506b948f42e885a5985591f6b46cca0faf1ec01
Youssef Mohamed Ali <95888292+JoeGitHubPro@users.noreply.github.com>
2024-02-04 15:59:08 +0200

    track static files

---
+++ TaskingSystem/Services/EmailSender.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net;
+using System.Net.Mail;
+
+namespace TaskingSystem.Services
+{
+    public class EmailSender : IEmailSender
+    {
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var fromMail = "youssef.mohamed.net.eg@gmail.com";
+            var fromPassword = "bccttblcwpxizydt";
+
+            var message = new MailMessage();
+            message.From = new MailAddress(fromMail);
+            message.Subject = subject;
+            message.To.Add(email);
+            var templatePath = Path.GetFullPath("wwwroot/StaticHTML/MailTemplate.html");
+            message.Body = File.ReadAllText(templatePath).ToString().Replace("ID-Confirm-1111-3333", htmlMessage.ToString());
+            // message.Body = _email.Replace("ID-Confirm-1111-3333", htmlMessage.ToString());
+            message.IsBodyHtml = true;
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(fromMail, fromPassword),
+                EnableSsl = true
+            };
+
+            smtpClient.Send(message);
+        }
+    }
+}
